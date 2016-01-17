b1339c72fdca755f7a707ac3032194758eb7a760
zmarty <contact@ovidiudan.com>
2016-01-16 22:46:32 -0800

    Renamed RemoveExtraSpacesTransformation transf
    Renamed RemoveExtraSpacesTransformation to
    RemoveExtraWhitespaceTransformation to match the name in the Node.js
    library.
    
    Also incremented NuGet package.

---
+++ OpenScraping/Transformations/RemoveExtraWhitespaceTransformation.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RemoveExtraWhitespaceTransformation.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenScraping.Transformations
+{
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using HtmlAgilityPack;
+
+    public class RemoveExtraWhitespaceTransformation : ITransformationFromHtml
+    {
+        public object Transform(Dictionary<string, object> settings, HtmlAgilityPack.HtmlNode node, List<HtmlAgilityPack.HtmlNode> logicalParents)
+        {
+            if (node != null)
+            {
+                var text = node.InnerText;
+
+                if (text != null)
+                {
+                    text = HtmlEntity.DeEntitize(text).Trim();
+                    text = Regex.Replace(text, @"\s\s+", " ");
+                    return text;
+                }
+            }
+
+            return null;
+        }
+    }
+}
