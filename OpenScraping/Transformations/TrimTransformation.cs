ddeab8bb45cd3c88b060090bfc34ea075b7aaa8f
zmarty <contact@ovidiudan.com>
2016-01-16 21:53:56 -0800

    Added TrimTransformation

---
+++ OpenScraping/Transformations/TrimTransformation.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TrimTransformation.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenScraping.Transformations
+{
+    using System.Collections.Generic;
+
+    public class TrimTransformation : ITransformationFromHtml
+    {
+        public object Transform(Dictionary<string, object> settings, HtmlAgilityPack.HtmlNode node, List<HtmlAgilityPack.HtmlNode> logicalParents)
+        {
+            if (node != null)
+            {
+                var text = node.InnerText;
+                return text.Trim();
+            }
+
+            return null;
+        }
+    }
+}
