0f9045302fb9434f3d0ddf76d1664e915b2038ff
Ovi Dan <zmarty@gmail.com>
2017-08-19 18:38:03 -0700

    .NET Core 2.0 support + updated dependencies

---
+++ OpenScraping/Transformations/ITransformationFromObject.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ITransformationFromContainer.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenScraping.Transformations
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json.Linq;
+
+    public interface ITransformationFromObject
+    {
+        object Transform(Dictionary<string, object> settings, object input);
+    }
+}
